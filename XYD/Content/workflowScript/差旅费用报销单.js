fc9e4d3620003553624cab5c181f417ffb509bc1
steven <1442498046@qq.com>
2020-01-06 22:43:11 +0800

    差旅费用映射数据OK

--- XYD/Content/workflowScript/差旅费用报销单.js
+++ XYD/Content/workflowScript/差旅费用报销单.js
@@ -36,7 +36,7 @@ function GetSerialSn(mid) {
             data.Data.records.forEach(function (item) {
                 serials.push(item.Sn);
             });
-            ShowUnitList("unit", "323px", "184px", "487px", "54px", "415px", serials, '#C-5-13', '#C-5-5', null);
+            ShowUnitList("unit", "323px", "184px", "487px", "54px", "415px", serials, '#C-5-13', '#C-5-5', MappingSourceData);
         }
     })
 }
@@ -78,10 +78,6 @@ function loadScripts(array, callback) {
 
 // 通用样式修改
 function AddCustomCss() {
-    // 修复弹出框被文件选择框挡住的问题
-    var styleTag = $('<style>.DropDown { z-index: 999; } .my-niban-opinion a, .my-opinion a {display:inline-block; margin-right: 10px;border: 1px solid #337ab7;font-size: 13px;padding: 3px 5px;margin-bottom: 10px;margin-left:15px;} .my-niban-opinion textarea, .my-opinion textarea { margin-top: 3px;margin-left: 15px;} #receiveNo > ul > li:hover,div#unit > ul > li:hover {background: rgb(207,229,239);}</style>')
-
-    $('html > head').append(styleTag);
     // 表单居中
     $("#page").css('display', 'flex');
     $("#page").css('width', '100%');
