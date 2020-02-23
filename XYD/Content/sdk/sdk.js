6742930103410d2223db6f95b71709d89d568d97
steven <1442498046@qq.com>
2020-02-23 14:55:24 +0800

    修复金额大写问题

--- XYD/Content/sdk/sdk.js
+++ XYD/Content/sdk/sdk.js
@@ -781,7 +781,7 @@ function MoneyToCapital(n) {
     }
     if (!/^(0|[1-9]\d*)(\.\d+)?$/.test(n))
         return "";
-    var unit = "千百拾亿千百拾万千百拾元角分", str = "";
+    var unit = "仟佰拾亿仟佰拾万仟佰拾元角分", str = "";
     n += "00";
     var p = n.indexOf('.');
     if (p >= 0)
@@ -789,7 +789,7 @@ function MoneyToCapital(n) {
     unit = unit.substr(unit.length - n.length);
     for (var i = 0; i < n.length; i++)
         str += '零壹贰叁肆伍陆柒捌玖'.charAt(n.charAt(i)) + unit.charAt(i);
-    return str.replace(/零(千|百|拾|角)/g, "零").replace(/(零)+/g, "零").replace(/零(万|亿|元)/g, "$1").replace(/(亿)万|壹(拾)/g, "$1$2").replace(/^元零?|零分/g, "").replace(/元$/g, "元整");
+    return str.replace(/零(仟|佰|拾|角)/g, "零").replace(/(零)+/g, "零").replace(/零(万|亿|元)/g, "$1").replace(/(亿)万|壹(拾)/g, "$1$2").replace(/^元零?|零分/g, "").replace(/元$/g, "元整");
 }
 
 function ConvertToCapitalCell(cell1, cell2) {
