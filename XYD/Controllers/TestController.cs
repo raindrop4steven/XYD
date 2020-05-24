4aaa2bfef25fbc3a4e04ccde83ff5c4aa7b7676c
steven <1442498046@qq.com>
2020-05-24 22:15:55 +0800

    事件接口跳转待修复好

--- XYD/Controllers/TestController.cs
+++ XYD/Controllers/TestController.cs
@@ -1,13 +1,41 @@
 using System;
+using System.IO;
 using System.Web.Mvc;
+using System.Web.Routing;
+using Appkiz.Library.Security;
+using Appkiz.Library.Security.Authentication;
+using Newtonsoft.Json;
+using XYD.Common;
+using XYD.Entity;
 
 namespace XYD.Controllers
 {
     public class TestController : Controller
     {
-        public ActionResult caculate()
+        [Authorize]
+        public ActionResult caculate(FormCollection collection)
         {
-            throw new Exception("that's cool");
+            return ResponseUtil.OK(new
+            {
+                employee = collection["employee"],
+                eventArgument = collection["eventArgument"]
+            });
+        }
+
+        [Authorize]
+        public ActionResult redirect()
+        {
+            var employee = (User.Identity as AppkizIdentity).Employee;
+
+            Stream stream = Request.InputStream;
+            stream.Seek(0, SeekOrigin.Begin);
+            string json = new StreamReader(stream).ReadToEnd();
+            var eventArguments = JsonConvert.DeserializeObject<XYD_Event_Argument>(json, new XYDCellJsonConverter());
+            return RedirectToAction("caculate", "Test", new
+            {
+                employee = employee,
+                eventArgument = eventArguments
+            });
         }
     }
 }
